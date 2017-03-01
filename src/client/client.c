81bc06e2fba2fd5c1aee7ceb2669b877507c1b75
Steeve DELARDIERE <sdelardi@e1r6p15.42.fr>
2017-03-01 22:47:19 +0100

    client and server are now connected

--- src/client/client.c
+++ src/client/client.c
@@ -6,7 +6,7 @@
 /*   By: sdelardi <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/03/01 21:49:02 by sdelardi          #+#    #+#             */
-/*   Updated: 2017/03/01 21:57:13 by sdelardi         ###   ########.fr       */
+/*   Updated: 2017/03/01 22:44:37 by sdelardi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,7 @@
 #include <unistd.h>
 #include <netdb.h>
 #include <arpa/inet.h>
+#include "../../libft/libft.h"
 
 int		create_client(char *addr, int port)
 {
@@ -42,12 +43,17 @@ int		main(int argc, char **argv)
 {
 	int 				port;
 	int 				sock;
+	int					res;
+	char				*buf;
 	
 	if (argc != 3)
 		return (0);
 	port = atoi(argv[2]);
 	sock = create_client(argv[1], port);
-	write(sock, "bonjour\n", 8);
+	while (((res = get_next_line(0, &buf)) >= 0))
+	{
+		write(sock, buf, ft_strlen(buf));
+	}
 	close(sock);
 	return (0);
 }
